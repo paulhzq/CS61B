7235a09122fdd26f3b4cca4a8fcf62956dd4c7c6
Paul Huang <paul_hzq@hotmail.com>
2017-02-18 11:48:02 -0600

    lab3 debug the Flik

--- lab3/Flik/Flik.java
+++ lab3/Flik/Flik.java
@@ -1,6 +1,7 @@
-/** An Integer tester created by Flik Enterprises. */
+/** An Integer tester created by Flik Enterprises. Integer.valueof(i)--> This method will always cache values in the range -128 to 127 */
+
 public class Flik {
-    public static boolean isSameNumber(Integer a, Integer b) {
+    public static boolean isSameNumber(Integer a,Integer b) {
         return a == b;
     }
 }
